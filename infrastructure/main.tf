fbf7d9f8b31b7ba0c24590ea6f55ecda193c69c0
Marline <marlinejohn@gmail.com>
2025-07-03 22:21:36 +0200

    Added monitoring setup with prometheus, grafana dashboards

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -78,8 +78,8 @@ module "eks_node_group" {
 
   name = "marline-expensy-nodes"
 
-  desired_size = 2
-  max_size     = 3
+  desired_size = 3
+  max_size     = 4
   min_size     = 1
 
   instance_types = ["t3.medium"]
